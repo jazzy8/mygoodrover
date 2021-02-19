c024014eccd73279b2fb3db30c19d93bb3a85bfc
jazzy8 <65190962+jazzy8@users.noreply.github.com>
2021-02-19 11:19:38 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,85 @@
+function reset_rover () {
+    led.enable(true)
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `).showImage(0, 50)
+    images.createImage(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `).showImage(0, 50)
+    images.createImage(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `).showImage(0, 50)
+    images.createImage(`
+        # # # # #
+        # # # # #
+        # # . # #
+        # # # # #
+        # # # # #
+        `).showImage(0, 50)
+    images.createImage(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `).showImage(0, 50)
+    images.createImage(`
+        # # # # #
+        # . . . #
+        # . # . #
+        # . . . #
+        # # # # #
+        `).showImage(0, 50)
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `).showImage(0, 50)
+    images.createImage(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `).showImage(0, 50)
+    images.createImage(`
+        # . # . #
+        . # # # .
+        # # # # #
+        . # # # .
+        # . # . #
+        `).showImage(0, 50)
+    images.createImage(`
+        # . # . #
+        . # # # .
+        # # . # #
+        . # # # .
+        # . # . #
+        `).showImage(0, 50)
+    images.createImage(`
+        # . # . #
+        . . . . .
+        # . . . #
+        . . . . .
+        # . # . #
+        `).showImage(0, 50)
+    basic.clearScreen()
+    control.reset()
+}
 radio.onReceivedValue(function (name, value) {
     if (name == "x") {
         x = value
@@ -5,12 +87,28 @@ radio.onReceivedValue(function (name, value) {
     if (name == "y") {
         y = value
     }
+    if (name == "z") {
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Half))
+        music.playTone(440, music.beat(BeatFraction.Double))
+    }
 })
 let y = 0
 let x = 0
 radio.setGroup(255)
+music.setBuiltInSpeakerEnabled(true)
+pins.setAudioPin(AnalogPin.P4)
+led.enable(false)
 x = 0
 y = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.pause(3000)
+        if (input.buttonIsPressed(Button.AB)) {
+            reset_rover()
+        }
+    }
+})
 basic.forever(function () {
     if (x == 0 && y > 0) {
         pins.analogWritePin(AnalogPin.P0, 768 + y)
